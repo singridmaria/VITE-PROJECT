9aa9acb62c6736a61a41bdcfd7fb251044209eb1
Visitante Digital <visitante.digital@DIGITALCOLLEGE.LOCAL>
2023-11-04 11:56:08 -0300

    Criando meu projetinho

---
+++ src/views/About.jsx
@@ -0,0 +1,13 @@
+import Title from "../components/title/Title"
+
+const About = () => {
+    return (
+        <>
+        <h2>Sobre</h2>
+        <Title title='Sobre'/>
+        
+        </>
+    )
+}
+
+export default About
---
+++ src/views/Banner.jsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+    return (
+        <>
+        <h2>Banner</h2>
+        
+        </>
+    )
+}
+
+export default Banner
